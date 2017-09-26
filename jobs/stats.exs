b601a84699331feb43fdb481088634121645a2e3
McAlex <mcalexvrn@yandex.ru>
2017-09-26 17:18:35 +0300

    godness view

--- jobs/stats.exs
+++ jobs/stats.exs
@@ -1,84 +1,46 @@
 use Kitto.Job.DSL
 
-job :job_failures, every: :second do
-  stats = Kitto.StatsServer.stats
-
-  failures = stats
-  |> Enum.map(fn ({name, m}) -> %{label: name, value: m[:failures]} end)
-  |> Enum.sort(fn (a, b) -> a[:value] > b[:value] end)
-  |> Enum.take(15)
-
-  broadcast! %{items: failures}
-end
-
-job :job_avg_time, every: {500, :milliseconds} do
-  stats = Kitto.StatsServer.stats
-
-  metrics = stats
-  |> Enum.map(fn ({name, m}) ->
-    %{label: name, value: m[:avg_time_took] |> Float.round(3) }
-  end)
-  |> Enum.sort(fn (a, b) -> a[:value] > b[:value] end)
-  |> Enum.take(15)
-
-  broadcast! %{items: metrics}
-end
-
-job :jobs_running, every: {200, :milliseconds} do
-  stats = Kitto.StatsServer.stats
-  |> Enum.filter(fn ({_name, m}) ->
-    (m[:times_completed] + m[:failures]) < m[:times_triggered]
-  end)
-  |> length
-
-  broadcast! %{value: stats}
-end
-
-job :footprint, every: :second do
-  mem = :erlang.memory[:processes_used] / 1024 / 1024 |> Float.round(3)
-
-  broadcast! :processes_count, %{value: length(:erlang.processes)}
-  broadcast! :memory_usage, %{value: mem}
-end
-
-job :uptime, every: :second do
-  hours = ((:erlang.statistics(:wall_clock) |> elem(0)) / 1000 / 60.0 / 60.0)
-  |> Float.round(3)
-
-  broadcast! %{value: hours}
-end
-
 job :rif_icoty, every: :hour do
-  html = HTTPotion.get "http://2017.rifvrn.ru/premia/works/decart-it-production-127.html"
+  url = "http://2017.rifvrn.ru/premia/works/decart-it-production-127.html"
+  html = HTTPotion.get url
   res = Floki.find(html.body, "#voteCounter") |> Floki.text
-  #{_, _, myarr} =  res
-  broadcast! %{text: "#{res} голосов", url: "http://2017.rifvrn.ru/premia/works/decart-it-production-127.html"}
+  resImg = Floki.find(html.body, ".b-premia-work-cover img") |> Floki.attribute("src")
+  imageUrl = resImg
+  broadcast! %{text: "#{res} голосов", url: url, imageUrl: imageUrl}
 end
 
 job :rif_our_site, every: :hour do
-  html = HTTPotion.get "http://2017.rifvrn.ru/premia/works/sayt-decart-it-production-170.html"
+  url = "http://2017.rifvrn.ru/premia/works/sayt-decart-it-production-170.html"
+  html = HTTPotion.get url
   res = Floki.find(html.body, "#voteCounter") |> Floki.text
-  #{_, _, myarr} =  res
-  broadcast! %{text: "#{res} голосов", url: "http://2017.rifvrn.ru/premia/works/sayt-decart-it-production-170.html"}
+  resImg = Floki.find(html.body, ".b-premia-work-cover img") |> Floki.attribute("src")
+  imageUrl = resImg
+  broadcast! %{text: "#{res} голосов", url: url, imageUrl: imageUrl}
 end
 
 job :rif_vtt_site, every: :hour do
-  html = HTTPotion.get "http://2017.rifvrn.ru/premia/works/internet-magazin-voronezhtorgtehnika-264.html"
+  url = "http://2017.rifvrn.ru/premia/works/internet-magazin-voronezhtorgtehnika-264.html"
+  html = HTTPotion.get url
   res = Floki.find(html.body, "#voteCounter") |> Floki.text
-  #{_, _, myarr} =  res
-  broadcast! %{text: "#{res} голосов", url: "http://2017.rifvrn.ru/premia/works/internet-magazin-voronezhtorgtehnika-264.html"}
+  resImg = Floki.find(html.body, ".b-premia-work-cover img") |> Floki.attribute("src")
+  imageUrl = resImg
+  broadcast! %{text: "#{res} голосов", url: url, imageUrl: imageUrl}
 end
 
 job :rif_sm_site, every: :hour do
-  html = HTTPotion.get "http://2017.rifvrn.ru/premia/works/sayt-seti-kinoteatrov-starmlad-135.html"
+  url = "http://2017.rifvrn.ru/premia/works/sayt-seti-kinoteatrov-starmlad-135.html"
+  html = HTTPotion.get url
   res = Floki.find(html.body, "#voteCounter") |> Floki.text
-  #{_, _, myarr} =  res
-  broadcast! %{text: "#{res} голосов", url: "http://2017.rifvrn.ru/premia/works/sayt-seti-kinoteatrov-starmlad-135.html"}
+  resImg = Floki.find(html.body, ".b-premia-work-cover img") |> Floki.attribute("src")
+  imageUrl = resImg
+  broadcast! %{text: "#{res} голосов", url: url, imageUrl: imageUrl}
 end
 
 job :rif_nik_site, every: :hour do
-  html = HTTPotion.get "http://2017.rifvrn.ru/premia/works/sayt-teatralnogo-centra-nikitinskiy-33.html"
+  url = "http://2017.rifvrn.ru/premia/works/sayt-teatralnogo-centra-nikitinskiy-33.html"
+  html = HTTPotion.get url
   res = Floki.find(html.body, "#voteCounter") |> Floki.text
-  #{_, _, myarr} =  res
-  broadcast! %{text: "#{res} голосов", url: "http://2017.rifvrn.ru/premia/works/sayt-teatralnogo-centra-nikitinskiy-33.html"}
+  resImg = Floki.find(html.body, ".b-premia-work-cover img") |> Floki.attribute("src")
+  imageUrl = resImg
+  broadcast! %{text: "#{res} голосов", url: url, imageUrl: imageUrl}
 end
--- widgets/text/text.js
+++ widgets/text/text.js
@@ -13,7 +13,8 @@ class Text extends Widget {
   render() {
     return (
     <a href={this.state.url}>
-      <div className={`${this.props.className} ${this.status()}`}>
+      <div className={`${this.props.className} ${this.status()}`} style={{backgroundImage: `url(${this.state.imageUrl})`,
+           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '350px'}}>
         <h1 className="title">{this.state.title || this.props.title}</h1>
         <h3>{this.state.text || this.props.text}</h3>
         <p className="more-info">{this.state.moreinfo || this.props.moreinfo}</p>
